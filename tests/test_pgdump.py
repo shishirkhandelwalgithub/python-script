cfa208c4f487d7ea7b9ce9984acbc3789a4aff9b
shishir khandelwal <shishir.khandelwal@sourcefuse.com>
2020-03-12 12:11:37 +0000

    newcommit

--- tests/test_pgdump.py
+++ tests/test_pgdump.py
@@ -9,7 +9,13 @@ def test_dump_calls_pg_dump(mocker):
 	Utilize pg_dump with the databse URL
 	"""
 	mocker.patch('subprocess.Popen')
-	
 	assert pgdump.dump(url)
 	subprocess.Popen.assert_called_with(['pg_dump',url],stdout=subprocess.PIPE)
 
+def test_dump_handles_oserror(mocker):
+	"""
+	pgdump.dump returns a reasonable error if pg_dump isn't installed
+	"""
+	mocker.patch('subprocess.Popen',side_effect=OSError("no such file"))
+	with pytest.raises(SystemExit):
+		pgdump.dump(url)
