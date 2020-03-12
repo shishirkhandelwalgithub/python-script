793034859615a9d5dea81007a975500abe8d5670
shishir khandelwal <shishir.khandelwal@sourcefuse.com>
2020-03-12 11:41:24 +0000

    mocks

---
+++ tests/test_pgdump.py
@@ -0,0 +1,15 @@
+import pytest
+import subprocess
+from pgbackup import pgdump
+
+url = "postgres://bob:password@example.com:5432/db_one"
+
+def test_dump_calls_pg_dump(mocker):
+	"""
+	Utilize pg_dump with the databse URL
+	"""
+	mocker.patch('subprocess.Popen')
+	
+	assert pgdump.dump(url)
+	subprocess.Popen.assert_called_with(['pg_dump',url],stdout=subprocess.PIPE)
+
